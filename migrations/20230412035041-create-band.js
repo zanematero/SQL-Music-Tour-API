10bdce96212a07ebf7fe0a0d49488c6d4f0bb34c
Zane Matero <materozane@gmail.com>
2023-04-12 14:44:01 -0700

    utilized sequelize cli to build database

---
+++ migrations/20230412035041-create-band.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Bands', {
+      band_id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      genre: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      available_start_time: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      end_time: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('band');
+  }
+};
